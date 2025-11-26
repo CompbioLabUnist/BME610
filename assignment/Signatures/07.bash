8d1209f1f1892a3e792ea306bc11c6a3d0b4891a
Jaewoong Lee <230@fumire.moe>
2025-11-26 17:26:38 +0900

    Add assignment
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- assignment/Signatures/07.bash
+++ assignment/Signatures/07.bash
@@ -7,13 +7,13 @@
 #SBATCH --mem=20G
 #SBATCH --export=ALL
 # DO NOT MODIFY the above lines
-mkdir -p input && ln -sfv "$(realpath ../12.pass.vcf)" "$(realpath input)"
+mkdir -p input && ln -sfv "$(realpath ../SAMPLE.PASS.vcf)" "$(realpath input)"
 
 python3 -c "from SigProfilerMatrixGenerator import install as genInstall; genInstall.install('GRCh38')"
-SigProfilerMatrixGenerator matrix_generator --exome '12' 'GRCh38' "$(realpath .)"
+SigProfilerMatrixGenerator matrix_generator --exome 'SAMPLE' 'GRCh38' "$(realpath .)"
 
 python3 -c "from SigProfilerExtractor import sigpro; sigpro.sigProfilerExtractor(input_type='vcf', input_data='$(realpath ./input)', output='$(realpath ./output)', reference_genome='GRCh38', exome=True, cpu=10, assignment_cpu=10)"
 
 rm -rfv Plot
-SigProfilerPlotting plotSBS --savefig_format 'png' --dpi 600 "$(realpath ./output/SBS/12.SBS96.exome)" "$(realpath Plot)" '12' '96'
-SigProfilerPlotting plotSBS --savefig_format 'png' --dpi 600 "$(realpath ./output/SBS/12.SBS6.exome)" "$(realpath Plot)" '12' '6'
+SigProfilerPlotting plotSBS --savefig_format 'png' --dpi 600 "$(realpath ./output/SBS/SAMPLE.SBS96.exome)" "$(realpath Plot)" 'SAMPLE' '96'
+SigProfilerPlotting plotSBS --savefig_format 'png' --dpi 600 "$(realpath ./output/SBS/SAMPLE.SBS6.exome)" "$(realpath Plot)" 'SAMPLE' '6'

eb52b32ba8b6a1182ba2786d057a1c901a0ce6f4
Jaewoong Lee <230@fumire.moe>
2025-11-26 17:26:48 +0900

    Add assignment
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- assignment/CNV/08.bash
+++ assignment/CNV/08.bash
@@ -7,9 +7,9 @@
 #SBATCH --mem=20G
 #SBATCH --export=ALL
 # DO NOT MODIFY the above lines
-cnvkit.py batch --processes 10 --normal "$(realpath ../12N.Sort.MarkDuplicates.BQSR.bam)" --fasta "/BiO/Share/Tools/gatk-bundle/hg38/Homo_sapiens_assembly38.fasta" --targets "/BiO/Share/Database/UCSC/hg38/UCSC_hg38.bed" --annotate "/BiO/Share/Database/UCSC/hg38/refFlat.txt"  --scatter --diagram --output-dir "$(realpath .)" "$(realpath ../12T.Sort.MarkDuplicates.BQSR.bam)"
-grep --invert-match '_' "$(realpath ./12T.Sort.MarkDuplicates.BQSR.cnr)" > "$(realpath 12T.cnr)"
-cnvkit.py segment --output 12T.cns --processes 10 12T.cnr
-cnvkit.py diagram --segment 12T.cns --output Diagram.pdf 12T.cnr && pdftoppm Diagram.pdf Diagram.png -png
-cnvkit.py scatter --segment 12T.cns --vcf "$(realpath ../12.PASS.vcf)" --output Scatter.pdf 12T.cnr && pdftoppm Scatter.pdf Scatter -png
-cnvkit.py heatmap --output Heatmap.pdf 12T.cns && pdftoppm Heatmap.pdf Heatmap -png
+cnvkit.py batch --processes 10 --normal "$(realpath ../NORMAL.Sort.MarkDuplicates.BQSR.bam)" --fasta "/BiO/Share/Tools/gatk-bundle/hg38/Homo_sapiens_assembly38.fasta" --targets "/BiO/Share/Database/UCSC/hg38/UCSC_hg38.bed" --annotate "/BiO/Share/Database/UCSC/hg38/refFlat.txt"  --scatter --diagram --output-dir "$(realpath .)" "$(realpath ../TUMOR.Sort.MarkDuplicates.BQSR.bam)"
+grep --invert-match '_' "$(realpath ./TUMOR.Sort.MarkDuplicates.BQSR.cnr)" > "$(realpath TUMOR.cnr)"
+cnvkit.py segment --output TUMOR.cns --processes 10 TUMOR.cnr
+cnvkit.py diagram --segment TUMOR.cns --output Diagram.pdf TUMOR.cnr && pdftoppm Diagram.pdf Diagram.png -png
+cnvkit.py scatter --segment TUMOR.cns --vcf "$(realpath ../SAMPLE.PASS.vcf)" --output Scatter.pdf TUMOR.cnr && pdftoppm Scatter.pdf Scatter -png
+cnvkit.py heatmap --output Heatmap.pdf TUMOR.cns && pdftoppm Heatmap.pdf Heatmap -png
--- assignment/CNV/2_install_dependency.bash
+++ assignment/CNV/2_install_dependency.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip3 install --requirement 2_requirements.txt
-Rscript --vanilla -e 'install.packages("BiocManager")' -e 'BiocManager::install("DNAcopy")'
+# Rscript --vanilla -e 'install.packages("BiocManager", repos="https://cloud.r-project.org")' -e 'BiocManager::install("DNAcopy")'
